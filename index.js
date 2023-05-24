eb95db1e74acbb4c99ef410a6063e9d7a0212100
farzanahoque2021 <farzanahoque2021@gmail.com>
2023-05-24 21:51:14 +0600

    Did delete operation

--- index.js
+++ index.js
@@ -104,6 +104,13 @@ async function run() {
             res.send(result);
         })
 
+        app.delete('/mytoys/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await carCollection.deleteOne(query);
+            res.send(result)
+        })
+
 
 
 
